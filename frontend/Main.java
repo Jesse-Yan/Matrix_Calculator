7148128b3367d80ceaa14da3bc23dbfe07710e86
Outlaw <54597560+JIangZeMingKuangTu@users.noreply.github.com>
2020-04-04 20:15:53 -0500

    Add frontend

---
+++ frontend/Main.java
@@ -0,0 +1,16 @@
+package frontend;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
