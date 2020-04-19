314bbfd1da5a75bf064e3cf4790ea7d9ceec58a2
”cbian03” <piensengxv@gmail.com>
2020-04-19 17:18:12 +0800

    parser update

--- src/application/Parser.java
+++ src/application/Parser.java
@@ -2,10 +2,10 @@ package application;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONReader;
 import com.alibaba.fastjson.annotation.JSONField;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 /**
  * This class parse from a JSON file
@@ -46,23 +46,42 @@ public class Parser {
     private String[] operations;
     public int matrix[][][];
 
+    public String readJsonFile(String filePath) {
+        String jsonStr = "";
+        try {
+            File jsonFile = new File(filePath);
+            FileReader fileReader = new FileReader(jsonFile);
+
+            Reader reader = new InputStreamReader(new FileInputStream(jsonFile), "utf-8");
+            int ch = 0;
+            StringBuffer sb = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                sb.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            jsonStr = sb.toString();
+            return jsonStr;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public Parser(String jsonFilepath) throws FileNotFoundException {
 
         // read the json file
         if (jsonFilepath == null) throw new FileNotFoundException();
-        File file = new File(jsonFilepath);
-        String fileText = file.toString();
-        JSONObject json = JSON.parseObject(fileText);
-        System.out.println(json.get("operations"));
-
-
+        String data = readJsonFile(jsonFilepath);
+        JSONObject json = JSONObject.parseObject(data);
+        System.out.println(json.get("Matrix"));
     }
 
     public static void main(String[] args) {
         try {
             Parser parser = new Parser("1.json");
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
 
     }
