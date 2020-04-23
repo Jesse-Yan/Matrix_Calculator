fd5778bc18314537df8d87d1f009a36bf0bf1197
Archer-Li <55112513+Archer-Li@users.noreply.github.com>
2020-04-23 11:58:06 -0500

    someone delete method in class, pleace check your pull before update your file

--- application/Matrix.java
+++ application/Matrix.java
@@ -132,6 +132,26 @@ public class Matrix implements MatrixADT {
     }
     return string;
   }
+  
+  /**
+   * Convert this matrix to string. The entries are converted to string by rows.
+   * In each row, numbers are separated by a " ". Rows are separated with ",\n".
+   */
+  public String toJsonString() {
+    String string = "";
+    for (int i = 0; i < entry.length; i++) {
+      if (i!=0)
+        string += ",\n";
+      string += "[";
+      for (int j = 0; j < entry[0].length; j++) {
+        if (j!=0)
+          string +=", ";
+        string += "\""+entry[i][j].toString() + "\"";
+      }
+      string += "]";
+    }
+    return string;
+  }
 
   /**
    * Return a 2D array of String to represent the matrix
