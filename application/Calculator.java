f92cc5339fe53e27de71b473e9d905c2fbb6d143
Outlaw <54597560+JIangZeMingKuangTu@users.noreply.github.com>
2020-04-27 16:55:33 -0500

    Update class structure

--- application/Calculator.java
+++ application/Calculator.java
@@ -1,10 +1,9 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 /**
- * This class computes the result of the given expression
+ * This class computes the result of the given complex expression
  * 
  * @author Jesse, Archer
  *
@@ -34,12 +33,12 @@ public class Calculator {
   }
 
   /**
-   * build up calculation expression and calculate result.
+   * Build up calculation expression and calculate result.
    * 
    * @param  expression String math expression
    * @return            result after calculation
    */
-  public static double calculate(String expression) {
+  private static double calculate(String expression) {
 
     expression = expression.replaceAll("(\\d+)\\(", "$1*(");
 
@@ -116,141 +115,10 @@ public class Calculator {
     if (temp.length() != 0)
       s.add(temp);
     String[] spliter = s.toArray(new String[0]);
-    ModifiedCal cal = new ModifiedCal(spliter);
+    SimpleCalculator cal = new SimpleCalculator(spliter);
     double result = cal.doCal();
     return result;
   }
 }
 
 
-class ModifiedCal {
-  private Stack<Double> theStackI;
-  private Stack<String> theStackP;
-  private String[] input;
-  private ArrayList<String> output;
-  private double num1, num2, interAns;
-
-  public ModifiedCal(String[] s) {
-    this.input = s;
-    theStackP = new Stack<String>();
-    output = new ArrayList<String>();
-  }
-
-  /**
-   * Calculation method that read input expression base on stack
-   * 
-   * @return result after process
-   */
-  public double doCal() {
-    doTrans();
-    doParse();
-    return theStackI.pop();
-  }
-
-  /**
-   * Read expression from stack and store result in stack
-   */
-  public void doParse() {
-    theStackI = new Stack<Double>();
-    for (int i = 0; i < output.size(); i++) {
-      String temp = output.get(i);
-      if (!(temp.equals("+") || temp.equals("-") || temp.equals("*")
-          || temp.equals("/"))) {
-        theStackI.push(Double.parseDouble(temp));
-      } else {
-        num2 = theStackI.pop();
-        num1 = theStackI.pop();
-        switch (temp) {
-          case "+":
-            interAns = num1 + num2;
-            break;
-          case "-":
-            interAns = num1 - num2;
-            break;
-          case "*":
-            interAns = num1 * num2;
-            break;
-          case "/":
-            interAns = num1 / num2;
-            break;
-          default:
-            interAns = 0;
-            break;
-        }
-        theStackI.push(interAns);
-      }
-    }
-  }
-
-  /**
-   * read and distribute calculation priority
-   */
-  public void doTrans() {
-    for (int i = 0; i < input.length; i++) {
-      String temp = input[i];
-      if (temp.equals("(")) {
-        theStackP.push(temp);
-      } else if (temp.equals("+") || temp.equals("-")) {
-        goOper(temp, 1);
-      } else if (temp.equals("*") || temp.equals("/")) {
-        goOper(temp, 2);
-      } else if (temp.equals(")")) {
-        goParen();
-      } else {
-        output.add(temp);
-      }
-    }
-
-    while (!theStackP.isEmpty()) {
-      output.add(theStackP.pop());
-    }
-  }
-
-  /**
-   * add sting in stackP into output if there's no bracket in stack
-   */
-  private void goParen() {
-
-    while (!theStackP.isEmpty()) {
-
-      String s = theStackP.pop();
-      if (s.equals("("))
-        break;
-      output.add(s);
-    }
-
-  }
-
-  /**
-   * helper method that process operations in level
-   * 
-   * @param temp -expression piece
-   * @param i    - level limit
-   */
-  private void goOper(String temp, int i) {
-
-    while (!theStackP.isEmpty()) {
-      String s = theStackP.pop();
-      if (s.equals("(")) {
-        theStackP.push(s);
-        break;
-      } else {
-        int j;
-
-        if (s.equals("+") || s.equals("-")) {
-          j = 1;
-        } else {
-          j = 2;
-        }
-
-        if (j < i) {
-          theStackP.push(s);
-          break;
-        } else {
-          output.add(s);
-        }
-      }
-    }
-    theStackP.push(temp);
-  }
-}
