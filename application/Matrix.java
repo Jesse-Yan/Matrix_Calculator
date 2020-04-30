283c4b14d728b37fe6ccdfc0c4d27d723fd605be
Archer-Li <55112513+Archer-Li@users.noreply.github.com>
2020-04-29 23:24:59 -0500

    add Screenshot

--- application/Matrix.java
+++ application/Matrix.java
@@ -4,9 +4,12 @@
 // Course: COMP SCI 400, Spring 2020
 //
 ///////////////////////////////// DESCRIPTION /////////////////////////////////
-//
-//
-//
+//This project ¡°Matrix calculator¡± by CS400 Ateam 2 aims to help students 
+//studying linear algebra to understand the calculations linear algebra better. 
+//This ¡°Matrix calculator¡± can not only do many matrix calculations like matrix 
+//multiplication, finding eigenvalues, and do LUP, QR, or Cholesky 
+//decompositions, but it can also support basic algebra calculations 
+//like a normal calculator and analyzing sequence. 
 //////////////////////////////////// CREDITS //////////////////////////////////
 //
 // Method to reduce a matrix to Hessenberg-form:
