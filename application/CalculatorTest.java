909b93fb2c23a8f7fa5fc206056ba28f579b62eb
Outlaw <54597560+JIangZeMingKuangTu@users.noreply.github.com>
2020-04-29 23:47:26 -0500

    Update comments

--- application/CalculatorTest.java
+++ application/CalculatorTest.java
@@ -15,30 +15,30 @@
 ///////////////////////////////// SOURCE FILES ////////////////////////////////
 //
 // Calculator.java, CalculatorTest.java, CalSteps.java, Fraction.java
-// Main.java, Matrix.java, MatrixADT.java, MatrixArithmeticException.java, 
+// Main.java, Matrix.java, MatrixADT.java, MatrixArithmeticException.java,
 // MatrixCalculator.java, MatrixDimensionsMismatchException.java,
-// MatrixTest.java, Numeric.java, NumericTest.java, OpeartionParser.java, 
-// SequenceSummary.java, SimpleCalculator.java, SingularException.java, 
+// MatrixTest.java, Numeric.java, NumericTest.java, OpeartionParser.java,
+// SequenceSummary.java, SimpleCalculator.java, SingularException.java,
 // Writer.java, styleSheet.css
 //
 ///////////////////////////////// DESCRIPTION /////////////////////////////////
 //
-// This project ¡°Matrix calculator¡± by CS400 Ateam 2 aims to help students
-// studying linear algebra to understand the calculations of linear algebra 
-// better. This ¡°Matrix calculator¡± can not only do many matrix calculations 
-// like matrix multiplication, finding eigenvalues, and LUP, QR, or Cholesky 
-// decompositions, but it can also support basic algebra calculations like a 
+// This project ï¿½ï¿½Matrix calculatorï¿½ï¿½ by CS400 Ateam 2 aims to help students
+// studying linear algebra to understand the calculations of linear algebra
+// better. This ï¿½ï¿½Matrix calculatorï¿½ï¿½ can not only do many matrix calculations
+// like matrix multiplication, finding eigenvalues, and LUP, QR, or Cholesky
+// decompositions, but it can also support basic algebra calculations like a
 // normal calculator and analyzing sequence.
-// 
-// This ¡°Matrix calculator¡± consists of two parts, a math calculator on the 
+//
+// This ï¿½ï¿½Matrix calculatorï¿½ï¿½ consists of two parts, a math calculator on the
 // left side, which supports basic algebra calculations and analyzing sequence,
-// and a matrix calculator on the right side, which supports calculations of 
+// and a matrix calculator on the right side, which supports calculations of
 // matrices.
 //
-// For matrix calculations, this ¡°Matrix calculator¡± also supports file inputs 
-// and outputs. The input files should be json files in a specific format, and 
-// the output files will also be json files. ¡°Matrix calculator¡± is also 
-// friendly to the computer user who does not have a keyboard. Users can input 
+// For matrix calculations, this ï¿½ï¿½Matrix calculatorï¿½ï¿½ also supports file inputs
+// and outputs. The input files should be json files in a specific format, and
+// the output files will also be json files. ï¿½ï¿½Matrix calculatorï¿½ï¿½ is also
+// friendly to the computer user who does not have a keyboard. Users can input
 // their data by clicking buttons provided on the user interface.
 //
 //////////////////////////////////// CREDITS //////////////////////////////////
@@ -63,6 +63,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 
 
@@ -76,6 +77,7 @@ public class CalculatorTest {
 
   /**
    * Before
+   * 
    * @throws Exception - unexpected
    */
   @Before
@@ -83,6 +85,7 @@ public class CalculatorTest {
 
   /**
    * After
+   * 
    * @throws Exception - unexpected
    */
   @After
@@ -92,11 +95,10 @@ public class CalculatorTest {
   /**
    * Test basic operations
    */
-  @SuppressWarnings("deprecation")
   @Test
   public void basic_test() {
     try {
-      assertEquals(Calculator.calcul("1+1*2"), 3.0, 0.0001);
+      assertEquals("Test Fail", Calculator.calcul("1+1*2"), 3.0);
     } catch (Exception e) {
       e.printStackTrace();
     }
