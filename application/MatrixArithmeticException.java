11e5843cd4febccca424c2b940b7bf02aee3779f
Archer-Li <55112513+Archer-Li@users.noreply.github.com>
2020-04-29 23:21:01 -0500

    header

--- application/MatrixArithmeticException.java
+++ application/MatrixArithmeticException.java
@@ -1,3 +1,23 @@
+//////////////////////////////// CS 400 HEADER ////////////////////////////////
+//
+// Title: Ateam project - Matrix Calculator
+// Course: COMP SCI 400, Spring 2020
+// Ateam: ateam2
+//
+///////////////////////////////// DESCRIPTION /////////////////////////////////
+//This project ¡°Matrix calculator¡± by CS400 Ateam 2 aims to help students 
+//studying linear algebra to understand the calculations linear algebra better. 
+//This ¡°Matrix calculator¡± can not only do many matrix calculations like matrix 
+//multiplication, finding eigenvalues, and do LUP, QR, or Cholesky 
+//decompositions, but it can also support basic algebra calculations 
+//like a normal calculator and analyzing sequence. 
+//////////////////////////////////// CREDITS //////////////////////////////////
+//
+// Method to reduce a matrix to Hessenberg-form:
+// https://math.stackexchange.com/questions/732924/reducing-a-matrix-to-upper-
+// hessenberg-form-using-householder-transformations-in
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 package application;
 
 /**
